ec8fb201de3b0c517be884cbed2fc6c1a39d47b2
offtheclockk <88730624+offtheclockk@users.noreply.github.com>
2022-03-10 15:43:02 -0800

    CarList, CarItem, and Header

--- App.js
+++ App.js
@@ -2,16 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 import CarItem from './components/CarItem';
+import CarList from './components/CarList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
-      <CarItem
-        name={"Model 3"}
-        tagline={"Order Online for "}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/Model3.jpeg')} />
+      <Header />
+      <CarList />
 
       <StatusBar style="auto" />
     </View>
